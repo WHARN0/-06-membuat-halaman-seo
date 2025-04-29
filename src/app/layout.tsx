84abf213fd474a94c4ed22d84ea4fef743cff5d3
WHARN0 <variosrio@gmail.com>
2025-04-30 05:00:22 +0700

    Fixxx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Layout>
           {children}
-          </Layout>
+        </Layout>
       </body>
     </html>
   );
