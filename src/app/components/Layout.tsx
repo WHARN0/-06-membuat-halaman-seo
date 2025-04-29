6291f825ec07780f7601b6247097ad7664341efe
WHARN0 <variosrio@gmail.com>
2025-04-30 05:07:44 +0700

    Verif Google

--- src/app/components/Layout.tsx
+++ src/app/components/Layout.tsx
@@ -25,6 +25,7 @@ export default function Layout({ children }: LayoutProps) {
   return (
     <>
       <Head>
+      <meta name="google-site-verification" content="VHOFIHgeHyK5PJQ2i-pHU9CjVenqbP9t1qm33i_HG6k" />
         <link rel="icon" href="/favicon.ico" />
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
